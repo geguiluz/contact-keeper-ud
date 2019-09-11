fb5473b60a9cbe2a33fc93d36f6402dc10efbda2
geguiluz <memo.eguiluz@gmail.com>
2019-09-11 15:38:28 -0500

    Token validation added to get contacts route

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const router = express.Router();
 
+const auth = require('../middleware/auth');
+const { check, validationResult } = require('express-validator/check');
+
+const User = require('../models/User');
+const Contact = require('../models/Contact');
+
 // @route     GET api/contacts
 // @desc      Get contacts for user
 // @access    Private
 
-router.get('/', (req, res) => {
-  res.send('Getting contacts');
+router.get('/', auth, async (req, res) => {
+  try {
+    // Get contacts ordered by date. Most recent first
+    const contacts = await Contact.find({ user: req.user.id }).sort({
+      date: -1,
+    });
+    res.json(contacts);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
 });
 
 // @route     POST api/contacts
